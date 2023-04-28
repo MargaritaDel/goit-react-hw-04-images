9ee02e7723f2eddea37bb4b48a23177242666c99
Margaryta Dieltsova <112723193+MargaritaDel@users.noreply.github.com>
2023-04-29 01:49:52 +0300

    fixed

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
+
 const URL = 'https://pixabay.com/api/';
 const KEY = '34025093-cc2dd49ea388fe86622ccaf7b';
 const PER_PAGE = 12;
 
 async function fetchImagesWithQuery(searchQuery, page) {
-  if (searchQuery) {
+  const trimmedQuery = searchQuery.trim();
+  if (trimmedQuery) {
     const response = await axios.get(
-      `${URL}?q=${searchQuery}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${PER_PAGE}`
+      `${URL}?q=${trimmedQuery}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${PER_PAGE}`
     );
     if (!response.status) {
       throw new Error(response.status);
@@ -15,4 +17,4 @@ async function fetchImagesWithQuery(searchQuery, page) {
   }
 }
 
-export default  fetchImagesWithQuery
+export default fetchImagesWithQuery;
